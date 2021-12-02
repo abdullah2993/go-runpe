40752649d49b5d953254aeeb8134eb76b5175e4a
nodauf <nodauf@krutt.org>
2021-12-02 11:48:22 +0100

    Use flag argument to create a new process with the flag CREATE_NEW_CONSOLE

--- cmd/exec/main.go
+++ cmd/exec/main.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	var src, dest string
+    var console bool
 	flag.StringVar(&src, "src", "C:\\Windows\\System32\\calc.exe", "Source executable")
-	flag.StringVar(&dest, "dest", "C:\\Windows\\System32\\notepad.exe", "Destenation executable")
+	flag.StringVar(&dest, "dest", "C:\\Windows\\System32\\notepad.exe", "Destination executable")
+	flag.BoolVar(&console, "console", false, "Create the process with the flag CREATE_NEW_CONSOLE (useful for process like cmd.exe)")
 	flag.Parse()
-	runpe.Inject(src, dest)
+	runpe.Inject(src, dest, console)
 }
